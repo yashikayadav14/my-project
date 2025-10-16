8a3edcf019a5cae36836cd6483b4d73ac7bbd829
yashikayadav14 <23bai70199@cuchd.in>
2025-10-16 21:15:30 +0530

    Create server.js

---
+++ FullStackIntegration/React_express_axios/Backeneed/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const products = [
+  { id: 1, name: "Laptop", price: 1200 },
+  { id: 2, name: "Mouse", price: 25 },
+  { id: 3, name: "Keyboard", price: 45 }
+];
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
